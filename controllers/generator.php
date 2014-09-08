1f80679748fd94001df2002c19c65a6e2c67179c
Jan-Hendrik Willms <tleilax+github@gmail.com>
2014-09-08 11:15:34 +0200

    add 3.2 as stud.ip version

--- controllers/generator.php
+++ controllers/generator.php
@@ -12,7 +12,7 @@
 			'StudienmodulManagementPlugin' => 'Studienmodulsuche',
 			'SystemPlugin'                 => 'Systemweite Erweiterungen',
 		),
-		'versions'   => array('2.0', '2.1', '2.2', '2.3', '2.4', '2.5','3.0','3.1'),
+		'versions'   => array('2.0', '2.1', '2.2', '2.3', '2.4', '2.5','3.0','3.1', '3.2'),
 	), request('plugin', array(
 		'assets'                    => '',
 		'css'                       => 'assets/style.css',
